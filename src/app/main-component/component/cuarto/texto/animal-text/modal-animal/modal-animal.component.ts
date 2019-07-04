15cac53b0a3ea2ad98a28546db996e99a8b8de34
Moisés Chiari <moiseschiari@MacBook-Pro-de-Moises.local>
2019-07-03 20:05:04 -0500

    modal con info

---
+++ src/app/main-component/component/cuarto/texto/animal-text/modal-animal/modal-animal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-animal',
+  templateUrl: './modal-animal.component.html',
+  styleUrls: ['./modal-animal.component.scss']
+})
+export class ModalAnimalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/main-component/component/cuarto/texto/animal-text/modal-fungi/modal-fungi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-fungi',
+  templateUrl: './modal-fungi.component.html',
+  styleUrls: ['./modal-fungi.component.scss']
+})
+export class ModalFungiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
