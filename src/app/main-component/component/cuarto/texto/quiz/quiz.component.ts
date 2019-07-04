4d0b6a38d73fdf1cbf857fa40e7d4203d61157f4
Madssimo <giancarlo.araujo@up.ac.pa>
2019-07-03 19:36:31 -0500

    Scrollbar en modal quiz

--- src/app/main-component/component/cuarto/texto/quiz/quiz.component.ts
+++ src/app/main-component/component/cuarto/texto/quiz/quiz.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  export class QuizComponent {
+    items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
   }
-
-}
